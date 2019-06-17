aed04a1c9ad8a3a605b8063598cacc073aeea460
Diego Sosa Díaz <diego.venezuela@gmail.com>
2019-06-17 09:35:07 +0200

    Adding minimal structure

---
+++ LESSON-03/katas/for.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * homework
+ * Build an iterator that executes from 0 to 100 printing iteration number
+ * but if iteration number is multiple of 3 print "foo" instead
+ * if iteration number is multiple of 5 print "bar" instead
+ * if iteration number is multiple of 5 and 3 at the same time, print "both";
+ * example output: 1, 2, foo, 4 , bar, .... 13, 14, both, ....
+ * hint: in math and in programming there is a concept called module, module refers to the remaining of a value
+ * example: in php 15%5 equals 0 because 15 / 5 has no remaining , 5*3 = 15
+ * other example  17%5 equals 2 because 17 / 5 has 2 as remaining, 5*3 + 2 = 17
+ * if (17%5) evaluates to true and  if (15%5) is false
+ * Remember that from this point you are a developer and your work will be read several times, don´t forget to indent your code
+ */
