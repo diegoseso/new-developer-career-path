e037b267c9eeac1c3bcc479113700b929aecaf65
marcelavbc <marcela.vilasboas@gmail.com>
2019-08-07 17:43:15 +0200

    working in the exercise

---
+++ LESSON-06/katas/solution/personalLibrary.php
@@ -0,0 +1,67 @@
+<?php
+
+
+class Book{
+    public $title;
+    public $author;
+    public $pageNumber;
+    public $sumary;
+    public $coverDescription;
+
+    public function __construct($title, $author, $pageNumber, $sumary, $coverDescription){
+        $this->title            = $title;
+        $this->author           = $author;
+        $this->pageNumber       = $pageNumber;
+        $this->sumary           = $sumary;
+        $this->coverDescription = $coverDescription;
+    }
+
+}
+
+class NovelBook extends Book{
+
+    public $literaryCurrent;
+
+    public function __construct($literaryCurrent, $title, $author, $pageNumber, $sumary, $coverDescription){
+        $this->literaryCurrent = $literaryCurrent;
+
+        parent::__construct($title, $author, $pageNumber, $sumary, $coverDescription);
+    }
+
+    public function getLiteraryCurrent(){
+        return $this->literaryCurrent;
+    }
+}
+
+class AdventureBook extends Book{
+
+
+}
+
+class BiographyBook extends Book{
+    
+    private $subjectName;
+
+    private function __construct ($subjectName, $title, $author, $pageNumber, $sumary, $coverDescription ){
+        $this->subjectName = $subjectName;
+
+        parent::__construct($title, $author, $pageNumber, $sumary, $coverDescription);
+    }
+
+    private function subjectName(){
+        return $this->subjectName;
+
+            private function 
+
+        
+    } 
+
+}
+
+
+
+
+
+
+
+ ?>
